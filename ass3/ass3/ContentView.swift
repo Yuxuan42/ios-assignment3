f6ac1a205d7b55921be70a8479377f7b2d0ff73e
Yuxuan42 <Yuxuan.Wang-5@student.uts.edu.au>
2025-05-14 11:28:09 +1000

    repo
    2

---
+++ ass3/ass3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ass3
+//
+//  Created by user941049 on 5/14/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ass3/ass3/ass3App.swift
@@ -0,0 +1,17 @@
+//
+//  ass3App.swift
+//  ass3
+//
+//  Created by user941049 on 5/14/25.
+//
+
+import SwiftUI
+
+@main
+struct ass3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
